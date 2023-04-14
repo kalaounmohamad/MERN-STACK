0c8ec828c6f176e7c5b625a78e71c74e2a82f90d
kalaounmohamad <kalaounmohamad@hotmail.com>
2023-04-15 02:57:06 +0300

    First Backend Commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
-const express = requires("express");
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+
+//Express App
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//Routes - this function is technically a middleware
+app.use("/api/workouts", workoutRoutes);
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //Listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
